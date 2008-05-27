c8496bd48f66696df7591c84f540295b9ff86517
Adam Sanderson <netghost@gmail.com>
2008-05-26 22:03:14 -0700

    StructureProcessor now scans classes and modules, this is in preparation for recursive structure diffs.

--- lib/structure_processor.rb
+++ lib/structure_processor.rb
@@ -1,104 +1,138 @@
-class MethodSignuature
-  def initialize name, classes=[], modules=[], instance=true
-    @name = name
-    @classes = classes.dup
-    @modules = modules.dup
-    @instance = instance
-  end
+class CodeObject
+  attr_reader :name
+  attr_reader :parent
+  attr_reader :children
+  attr_reader :sexp
   
-  def to_s
-    scope = (@modules + @classes).join("::")
-    if @instance
-      "#{scope}##{@name}"
-    else
-      "#{scope}.#{@name}"
+  def initialize(name, parent, sexp)
+    @name = name
+    @parent = parent
+    @sexp = sexp.deep_clone
+    @children = []
+    if parent
+      parent.children << self
     end
   end
   
-  # The following are all just to quickly get hashing right
-  # TODO: replace with real found_methods
-  def hash
-    self.to_s.hash
+  def signature
+    self.name
   end
   
-  def <=> other
-    self.to_s <=> other.to_s
+  def to_s
+    signature
   end
   
-  def == other
-    self.to_s == other.to_s
+  def ==(other)
+    return false unless other.class == self.class
+    (other.signature == self.signature) and (other.sexp == self.sexp)
+  end
+end
+
+class ModuleCode < CodeObject
+  def signature
+    parent_signature = self.parent ? self.parent.signature : nil
+    [parent_signature,self.name].compact.join('::')
+  end
+end
+
+class ClassCode < ModuleCode
+end
+
+class MethodCode < CodeObject
+  def initialize(name, parent, instance, sexp)
+    super(name, parent, sexp)
+    @instance = instance
   end
   
-  def eql? other
-    self.to_s.eql? other.to_s
+  def signature
+    parent_signature = self.parent ? self.parent.signature : ""
+    [parent_signature,self.name].join( @instance ? '#' : ".")
   end
 end
 
 class StructureProcessor < SexpProcessor
-  attr_accessor :found_methods
+  attr_accessor :code_objects
+  attr_accessor :root_objects
+  
+  attr_accessor :scope_stack
   
   def initialize
     super
     self.strict = false
     self.auto_shift_type = true
-    @classes = []
-    @modules = []
+    
     @instance_scope = true
-    @found_methods = {}
+    @code_objects = {}
+    @root_objects = {}
+    @scope_stack = []
   end
   
   def process_class(exp)
     name = exp.shift
     super_class = exp.shift #?
     body = exp.shift
-    @classes.unshift name
-    result = s(:class, name, process(super_class), process(body))
-    @classes.shift
-    return result
+    
+    record ClassCode.new(name, self.scope, body) do
+      s(:class, name, process(super_class), process(body))
+    end
   end
   
   def process_module(exp)
     name = exp.shift
     body = exp.shift
-    @modules.unshift name
-    result = s(:class, name, process(body))
-    @modules.shift
-    return result
+    
+    record ModuleCode.new(name, self.scope, body) do
+      s(:class, name, process(body))
+    end
   end
   
   def process_defn(exp)
     name = exp.shift
-    args = process exp.shift
     body = process exp.shift
-
-    signature = MethodSignuature.new(name, @classes, @modules, @instance_scope)
-    @found_methods[signature] = {:name=>name, :args=>args, :body=>body}
     
-    return s(:defn, name, args, body)
+    record MethodCode.new(name, self.scope, @instance_scope, body) do
+      s(:defn, name, body)
+    end
   end
   
   def process_defs(exp)
-    
-    scope = process exp.shift
+    exp_scope = process exp.shift
     name = exp.shift
-    args = process exp.shift
     body = process exp.shift
-
-    signature = MethodSignuature.new(name, @classes, @modules, false)
-    @found_methods[signature] = {:name=>name, :args=>args, :body=>body}
     
-    return s(:defs, scope, name, args, body)
+    record MethodCode.new(name, self.scope, false, body) do
+      s(:defs, exp_scope, name, body)
+    end
   end
   
   def process_sclass(exp)
     @instance_scope = false
-    scope = process exp.shift
+    exp_scope = process exp.shift
     body = process exp.shift
     @instance_scope = true
-    return s(:sclass, scope, body)
+    
+    s(:sclass, exp_scope, body)
   end
   
   def diff(other_processor)
-    method_diff = CodeComparison.new(self.found_methods, other_processor.found_methods).changes
+    method_diff = CodeComparison.new(self.code_objects, other_processor.code_objects).changes
+  end
+  
+  protected
+  def record obj
+    signature = obj.signature
+    if !self.code_objects[signature]
+      self.code_objects[signature] = obj
+      self.root_objects[signature] = obj if obj.parent == nil
+    end
+    
+    self.scope_stack << self.code_objects[signature]
+    result = yield if block_given?
+    self.scope_stack.pop
+    result
+  end
+  
+  def scope
+    self.scope_stack.last
   end
 end
